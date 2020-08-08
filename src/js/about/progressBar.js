5a057674a4a08545e7196e9d0668d984290fd147
Kamlesh patil <kpkamleshpatil333@gmail.com>
2020-08-08 21:09:13 +0530

    hopefully it works

--- src/js/about/progressBar.js
+++
@@ -1,6 +0,0 @@
-import {d, fadeClass} from '../common/util';
-export default function () {
-  //プログレスバーのアニメーション用
-  const progressBar = d.querySelectorAll('.js_progressBar');
-  fadeClass(progressBar, progressBar, 'add', '');
-};
